115434cb5c19e76893ae45ab3953789882896621
Gedalya Krycer <gedalya@krycer.com>
2021-03-29 18:17:13 -0700

    Add SocialMediaIcon.js component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import SocialMediaIcon from "../ui/SocialMediaIcon/SocialMediaIcon";
 
 const Navbar = props => {
     return (
-        <nav>Navbar</nav>
+        <nav>
+            Navbar
+            <SocialMediaIcon
+                type={"facebook"}
+                location={"contact"}
+                link={"https://www.google.com"}
+                height={23}
+                width={23}
+                viewBoxSize="0 0 23 23"
+                disable={false}
+            />
+        </nav>
     );
 }
 
