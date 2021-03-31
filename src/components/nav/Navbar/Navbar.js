a6a3f2c9efcf83514fb91eab79a55d871488fd82
Gedalya Krycer <gedalya@krycer.com>
2021-03-31 12:33:33 -0700

    Add responsive styles

--- src/components/nav/Navbar/Navbar.js
+++ src/components/nav/Navbar/Navbar.js
@@ -1,25 +1,67 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './navbarStyles.scss'
 import SocialMediaIcon from "../../ui/SocialMediaIcon/SocialMediaIcon";
 import PrimaryCTAButton from "../../ui/PrimaryCTAButton/PrimaryCTAButton";
+import Logo from '../../../assets/ghs-ribbon-logo.svg'
+import useWindowDimensions from '../../../utils/useWindowDimensions'
 
 const Navbar = props => {
+
+    const [openMenu, setOpenMenu] = useState(false);
+    const [desktopView, setDesktopView] = useState(true);
+    const {width} = useWindowDimensions()
+
+    useEffect(() => {
+        if(width > 676) {
+            setDesktopView(true);
+        } else {
+            setDesktopView(false);
+        }
+    }, [width])
 
     const mailchimpModalHandler = () => {
         alert("Modal clicked")
     }
 
     return (
-        <nav className="navBar">
-            <div className="temp-logo">Logo</div>
-            <ul className="navbar__links">
+        <nav
+            role="navigation"
+            className={`navBar ${openMenu && width < 677  ? "navBar--active" : null}`}
+        >
+
+            <div className="navBar__header">
+                <img
+                    className="navBar__logo"
+                    src={Logo}
+                    alt="Gen Hybrid Systems logo"
+                />
+
+                <button
+                    className={`navBar__toggle ${openMenu ? "open-menu" : null}`}
+                    aria-controls="menu"
+                    aria-expanded={false}
+                    onClick={() => setOpenMenu(curValue => !curValue)}
+                >
+                    <span className="navBar__toggle-lines-1" />
+                    <span className="navBar__toggle-lines-2" />
+                    <span className="navBar__toggle-lines-3" />
+                    <span className="screen-reader-text">Menu</span>
+                </button>
+            </div>
+
+            <ul
+                className={`navbar__links ${openMenu || desktopView ? null : "hidden"}`}
+                id="menu"
+            >
                 <li><a href="#crisis">crisis</a></li>
                 <li><a href="#mission">mission</a></li>
                 <li><a href="#videos">videos</a></li>
                 <li><a href="#connect">connect</a></li>
             </ul>
 
-            <div className="navbar__social-container">
+            <div
+                className={`navbar__social-container ${openMenu ? null : "hidden"}`}
+            >
                 <SocialMediaIcon
                     type={"youtube"}
                     location={"mobile-nav"}
@@ -64,7 +106,9 @@ const Navbar = props => {
                 />
             </div>
 
-            <div className="navbar__cta-container">
+            <div
+                className={`navbar__cta-container ${openMenu || desktopView ? null : "hidden"}`}
+            >
                 <PrimaryCTAButton
                     handleClick={mailchimpModalHandler}
                     label="get updates"
