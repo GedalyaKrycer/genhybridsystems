82c8db395afe14f728b7f012ad92c95eb3c25c97
Gedalya Krycer <gedalya@krycer.com>
2021-04-02 12:07:14 -0700

    Add image animations and base text content

---
+++ src/components/header/HeroContainer/HeroContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './heroStyles.scss';
+import SectionLayout from "../../ui/SectionLayout/SectionLayout";
+
+
+const HeroContainer = props => {
+
+
+    return (
+        <>
+            <SectionLayout customClass="hero__container">
+                <div className="hero__content">
+                    <h1>Education on <span className="g__text-emphasis">their</span> terms brings hope</h1>
+                    <p className="hero__subhead">Our hybrid classroom and at-home learning model makes education accessible in underserved communities. </p>
+                </div>
+
+            </SectionLayout>
+
+
+            <div className="hero__img-container">
+                <div className="hero__bg-img hero__img-1" />
+                <div className="hero__bg-img hero__img-2"/>
+                <div className="hero__bg-img hero__img-1"/>
+                <div className="hero__bg-img hero__img-2"/>
+                <div className="hero__bg-img hero__img-1"/>
+            </div>
+        </>
+    );
+}
+
+export default HeroContainer;
