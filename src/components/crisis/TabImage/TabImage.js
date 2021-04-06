f16d4f494baaf0a1e3d085206369f19d4d86fa59
Gedalya Krycer <gedalya@krycer.com>
2021-04-06 01:22:01 -0700

    Add new image animation

--- src/components/crisis/TabImage/TabImage.js
+++ src/components/crisis/TabImage/TabImage.js
@@ -9,7 +9,10 @@ const TabImage = props => {
 
         <div
             className="tab-image"
-            style={{background: `linear-gradient(270deg, #39251C 0%, rgba(57, 37, 28, 0.4) 80%), url(${props.image})`}} />
+            style={{
+                background: `linear-gradient(270deg, #39251C 0%, rgba(57, 37, 28, 0.4) 80%), url(${props.image}) no-repeat`,
+                backgroundSize: 'cover'
+            }} />
 
     );
 
