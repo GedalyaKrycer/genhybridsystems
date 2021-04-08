93767145af119c8167783a68ce06bf62bf9de2be
Gedalya Krycer <gedalya@krycer.com>
2021-04-08 16:09:52 -0700

    Update validation for email fields and added textarea

--- src/components/ui/InputField/InputField.js
+++ src/components/ui/InputField/InputField.js
@@ -3,17 +3,45 @@ import './inputFieldStyles.scss';
 
 const InputField = props => {
 
+    //Checks if all the fields are filled and if an @ sign is used in the email field
+    const validateInput = values => {
+
+        if (values.some(f => f === "") || values[0].indexOf("@") === -1) {
+            console.log("True was hit")
+            return true
+        } else {
+            console.log("FALSE was hit")
+            return false
+        }
 
 
+    }
     if (props.type === "submit") {
+        console.log("props.formValues")
+        console.log(props.formValues[0].indexOf("@") === -1)
         return (
             <input
                 className='primaryBtn primaryBtn--big g__justify-self-center'
                 type='submit'
                 value={props.label}
-                disabled={props.formValues && props.formValues.some(f => f === "")}
+                disabled={validateInput(props.formValues)}
             />
         )
+    } else if (props.type === "textarea") {
+        return (
+            <label className="inputField__label">
+                {props.label}
+                <textarea
+                    onChange={(e) => props.onChangeHandler(e.target.value)}
+                    placeholder={props.placeholder}
+                    value={props.value}
+                    required={props.isRequired}
+                    className="inputField__field"
+                    rows={7}
+                    name={props.name}
+                />
+            </label>
+        );
     } else {
         return (
             <label className="inputField__label">
@@ -25,6 +53,7 @@ const InputField = props => {
                     value={props.value}
                     required={props.isRequired}
                     className="inputField__field"
+                    name={props.name}
                 />
             </label>
         );
